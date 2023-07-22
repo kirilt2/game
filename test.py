b79333e19210db8b5cfee587db83257918bc8978
KirilT <kiril.tx07@gmail.com>
2023-07-22 13:20:40 +0300

    timezone
    dd

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
-import datetime
 import pytz
-
+from datetime import datetime
 
 def get_israel_time():
-    # Get the current UTC time
-    utc_time = datetime.datetime.utcnow()
+    # Set the timezone for Israel (IST, UTC+3)
+    israel_tz = pytz.timezone('Asia/Jerusalem')
 
-    # Get the Israel time zone
-    israel_tz = pytz.timezone('Israel')
+    # Get the current time in UTC
+    utc_now = datetime.utcnow()
 
     # Convert the UTC time to Israel time
-    israel_time = utc_time.astimezone(israel_tz)
-    return israel_time
+    israel_now = utc_now.replace(tzinfo=pytz.utc).astimezone(israel_tz)
 
-# Get the current time in Israel
-current_time_israel = get_israel_time()
-print(f"Current time in Israel!@: {current_time_israel.strftime('%Y-%m-%d %H:%M:%S')}")
+    return israel_now
+
+if __name__ == "__main__":
+    israel_time = get_israel_time()
+    print("Current time in Israel:", israel_time.strftime("%Y-%m-%d %H:%M:%S"))
