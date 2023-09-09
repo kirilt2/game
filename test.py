25fd10b704c3a4340e513c112dddb7906d01e6e8
KirilT <kiril.tx07@gmail.com>
2023-09-09 17:25:50 +0300

    d
    d

--- test.py
+++ test.py
@@ -1,18 +1,38 @@
-import pytz
-from datetime import datetime
+import asyncio
+from telegram import Bot, Update, ForceReply, InputFile
+from telegram.ext import Updater, CommandHandler, Application, ContextTypes
 
-def get_israel_time():
-    # Set the timezone for Israel (IST, UTC+3)
-    israel_tz = pytz.timezone('Asia/Jerusalem')
+bot_token = '6660902386:AAGbERi4TQa4MWFSfe2AdaEFRt9X49RcA2s'
 
-    # Get the current time in UTC
-    utc_now = datetime.utcnow()
+bot = Bot(token=bot_token)
 
-    # Convert the UTC time to Israel time
-    israel_now = utc_now.replace(tzinfo=pytz.utc).astimezone(israel_tz)
+class MyBot:
+    def __init__(self, bot_token):
+        self.application = Application.builder().token(bot_token).build()
 
-    return israel_now
+        # Register command handlers
+        self.application.add_handler(CommandHandler("start", self.start))
+        self.application.add_handler(CommandHandler("help", self.help))
+        self.application.add_handler(CommandHandler("memes", self.send_multiple_memes))
+        self.application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        user = update.effective_user
+        await update.message.reply_text(
+            f"Hi {user.mention_html()}!",
+            reply_markup=ForceReply(selective=True),
+        )
+
+    async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text(text="Это простой бот. Он может отвечать на текстовые сообщения.")
+
+    async def send_multiple_memes(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send multiple memes when the /memes command is issued."""
+        memes = ['meme1.jpeg', 'meme2.jpg', 'meme.jpeg', 'meme1.jpeg', 'meme2.jpg', 'meme.jpeg', 'meme1.jpeg', 'meme2.jpg', 'meme.jpeg', 'meme1.jpeg', 'meme2.jpg', 'meme.jpeg','meme1.jpeg', 'meme2.jpg', 'meme.jpeg','meme1.jpeg', 'meme2.jpg', 'meme.jpeg','meme1.jpeg', 'meme2.jpg', 'meme.jpeg','meme1.jpeg', 'meme2.jpg', 'meme.jpeg']  # Add more meme file names if needed
+
+        for meme_file_name in memes:
+            with open(meme_file_name, 'rb') as meme_file:
+                await update.message.reply_photo(photo=InputFile(meme_file))
 
 if __name__ == "__main__":
-    israel_time = get_israel_time()
-    print("Current time in Israel:", israel_time.strftime("%Y-%m-%d %H:%M:%S"))
+    bot = MyBot(bot_token=bot_token)
